dd19566670582a9ec2111e1a93f247a0aec75b08
kabamaru2372 <fotis.poggas@gmail.com>
2026-03-24 15:14:57 +0100

    Initial Commit

---
+++ UnPluq/UnPluqApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnPluqApp.swift
+//  UnPluq
+//
+//  Created by Fotios Pongas on 24.03.26.
+//
+
+import SwiftUI
+
+@main
+struct UnPluqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
